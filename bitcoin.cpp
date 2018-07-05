b7fa4d9ccaaea944ce3167c7be2ac8641df6681b
Slate Random Dev <slate@i2pmail.org>
2018-07-05 07:05:23 +0200

    Add Slates settings 3

--- bitcoin.cpp
+++ bitcoin.cpp
@@ -298,7 +298,7 @@ bool TestNode(const CService &cip, int &ban, int &clientV, std::string &clientSV
 
 
 int main(void) {
-  CService ip("slateseeder.xhost.xyz", 37415, true);
+  CService ip("slateseeder1.xhost.xyz", 37415, true);
   vector<CAddress> vAddr;
   vAddr.clear();
   int ban = 0;
